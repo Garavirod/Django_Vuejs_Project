17cf385ea8515a750f2e091760695467a7d434fd
Garavirod <rodrigogarciaavila26@gmail.com>
2020-04-23 18:38:58 -0500

    dis en django

--- BooksDjVue/BooksDjVue/urls.py
+++ BooksDjVue/BooksDjVue/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Here we register the API routes, we define the veriosn of the apithat is a good practice
-    path('api/v1.0/',include('book.urls'))
+    path('api/v1.0/',include('book.urls')),
+    url(r'^$',TemplateView.as_view(template_name="index.html")),
 ]
